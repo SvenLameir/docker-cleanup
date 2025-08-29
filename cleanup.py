6b79783d91973e7759cead52cd7bd5ece9609f76
SvenLameir <66329101+SvenLameir@users.noreply.github.com>
2025-08-29 17:40:15 +0200

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -3,11 +3,14 @@ import subprocess
 import time
 
 def prune_images():
-   image_age = os.environ.get('IMAGE_AGE', '24h')
+    image_age = os.environ.get('IMAGE_AGE', '24h')
     print(f"Pruning images older than {image_age}...")
-    subprocess.run([
-        'docker', 'image', 'prune', '-a', '--force', f'--filter=until={image_age}'
-    ], check=True)
+    try:
+        subprocess.run([
+            'docker', 'image', 'prune', '-a', '--force', f'--filter=until={image_age}'
+        ], check=True)
+    except FileNotFoundError:
+        print("Error: Docker is not installed or not in PATH.")
 
 def main():
     # How often to run the cleanup, in seconds (default: 3600 = 1 hour)
